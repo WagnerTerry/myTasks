7862a13b5d2d3793faa75ba887256bdcc2d7479f
Wagner Terry <goncalveswagner15@gmail.com>
2023-08-30 17:08:07 -0300

    feat: Criando metodo para remover as tasks

--- src/components/TaskList/index.tsx
+++ src/components/TaskList/index.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
-import { FlatList, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { Alert, FlatList, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import { ITask, useTaskList } from '../../context/TasksContent'
 
 export const TaskList = () => {
-  const { tasks } = useTaskList()
+  const { tasks, removeTask } = useTaskList()
+
+  const handleRemoveTask = (id: string) => {
+    Alert.alert('Tem certeza?', 'Deseja realmente excluir a tarefa', [
+      {
+        text: "Cancelar",
+        onPress: () => { }
+      },
+      {
+        text: "Excluir",
+        onPress: () => removeTask(id)
+      }
+    ])
+  }
+
   return (
 
     < FlatList
@@ -11,7 +25,10 @@ export const TaskList = () => {
       data={tasks as unknown as ITask[]}
       keyExtractor={item => item.id}
       renderItem={({ item }) => (
-        <TouchableOpacity style={styles.buttonTask}>
+        <TouchableOpacity
+          onPress={() => handleRemoveTask(item.id)}
+          style={styles.buttonTask}
+        >
           <Text style={styles.titleTask}>{item.title}</Text>
         </TouchableOpacity>
       )}
